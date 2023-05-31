a2b385f0e53eae846cb70eac4fed5074b6b81650
BryanLMacMurrayOverdrive <bmacmurray@overdriveinteractive.com>
2023-05-31 14:10:55 -0400

    added Agent and Interface Files
    Uploaded Agent and Interface Files.

---
+++ agent.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 12:25:49 2023
+
+@author: bmacmurray
+"""
+
+# agent.py
+from langchain import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
+import pandas as pd
+
+# Setting up the api key
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+API_KEY = env("apikey")
+
+
+def create_agent(filename: str):
+    """
+    Create an agent that can access and use a large language model (LLM).
+
+    Args:
+        filename: The path to the CSV file that contains the data.
+
+    Returns:
+        An agent that can access and use the LLM.
+    """
+    
+    # Create an OpenAI object.
+    llm = OpenAI(openai_api_key=API_KEY, temperature=0.2)
+
+    # Read the CSV file into a Pandas DataFrame.
+    #Date columns need to be passed into the "parse_dates" argument in an array
+    # You can specify data types as a dict using the "dtypes" argument in the pandas.read_csv function. 
+    df = pd.read_csv(filename,
+    dtype={
+    
+'Total users':'int64',
+'Conversions':'int64',
+'Events per session':'float64'
+
+    },
+	parse_dates=['Date'])
+
+    df.info()
+
+    # Create a Pandas DataFrame agent.
+    return create_pandas_dataframe_agent(llm, df, verbose=True)
+
+def query_agent(agent, query):
+    """
+    Query an agent and return the response as a string.
+
+    Args:
+        agent: The agent to query.
+        query: The query to ask the agent.
+
+    Returns:
+        The response from the agent as a string.
+    """
+
+    prompt = (
+        """
+            For the following query, if it requires drawing a table, reply as follows:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            If the query requires creating a bar chart, reply as follows:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            If the query requires creating a line chart, reply as follows:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            There can only be two types of chart, "bar" and "line".
+
+            If it is just asking a question that requires neither, reply as follows:
+            {"answer": "answer"}
+            Example:
+            {"answer": "The title with the highest rating is 'Gilead'"}
+
+            If you do not know the answer, reply as follows:
+            {"answer": "I do not know."}
+
+            Return all output as a string.
+
+            All strings in "columns" list and data list, should be in double quotes,
+
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+
+            Lets think step by step.
+
+            Below is the query.
+            Query: 
+            """
+        + query
+    )
+
+    # Run the prompt through the agent.
+    response = agent.run(prompt)
+
+    # Convert the response to a string.
+    return response.__str__()
